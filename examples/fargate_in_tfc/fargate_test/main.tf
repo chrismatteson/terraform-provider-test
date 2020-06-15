16d468228b11993b765d83d36fa613ed42c017eb
Chris Matteson <chris.matteson@gmail.com>
2020-06-15 15:54:22 -0700

    added null resource

--- examples/fargate_in_tfc/fargate_test/main.tf
+++ examples/fargate_in_tfc/fargate_test/main.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "fargate" {
+data terraform_remote_state "fargate" {
   backend = "remote"
 
   config = {
@@ -13,5 +13,11 @@ data test_scenario "fargate" {
   step {
     program = ["curl", "-o", "/dev/null", "-s", "-w", "{\"response_code\":\"%%{http_code}\"}", "http://54.241.103.180/"]
     expect = { "response_code" = "200" }
+  }
+}
+
+resource null_resource "trigger" {
+  triggers = {
+    trigger = data.test_scenario.fargate.result
   }
 }
